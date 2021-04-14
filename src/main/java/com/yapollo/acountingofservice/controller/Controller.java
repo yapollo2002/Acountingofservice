f59f074414b73fd1de005e9cc06703995db52e15
yapollo2002 <yapollo2002@yahoo.com>
2021-04-14 08:31:24 +0300

    first method of controller have been added

--- src/main/java/com/yapollo/acountingofservice/controller/Controller.java
+++ src/main/java/com/yapollo/acountingofservice/controller/Controller.java
@@ -1,4 +1,30 @@
 package com.yapollo.acountingofservice.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
 public class Controller {
+
+
+
+       @GetMapping(path="/all")
+       public List find(){
+           return null;
+       }
+
+       @GetMapping(path="/find/{phonenumber}")
+       public List findPhonenumber(@PathVariable(value="phonenumber") int phonenumber){
+           return null;
+       }
+
+       @PostMapping(path="/")
+       public void create(){
+           return null;
+       }
+
 }
